5b51697b268365eae0edb627147243569b51564b
Orban Abel <orban@abel.ro>
2022-10-06 00:07:56 +0300

    screen clearing now separate function, renamed some variables to their in-game names, cleaned up color display a bit

--- spider_solitaire.py
+++ spider_solitaire.py
@@ -20,19 +20,22 @@ t_colours = {
 
 nTotalUnits = 8
 nTotalColumns = 10
-nTotalHelpDecks = 4
+nTotalDeals = 4
 
 t_columns = []
-t_helpDecks = [] # the decks usually in the bottom right corner
+t_deals = []     # the decks usually in the bottom right corner
 t_allCards = []  # where every card is before we sort them into decks
 
-def display():
+def clearScreen():
     os.system("cls" if os.name == "nt" else "clear")
+
+def display():
+    clearScreen()
     nMaxCards = max([len(col) for col in t_columns])
 
     row = "   "    
     for i in range(nTotalColumns):
-        row += str(i + 1) + "  "
+        row += t_colours[10] + str(i + 1) + "  "
 
     print(row)
     for i in range(nMaxCards):
@@ -51,14 +54,15 @@ def display():
 
     print("")
     print("Completed Units: {}".format(nCompletedUnits))
-    print("Help Decks: {}".format(nHelpDecks))
+    print("Deals: {}".format(nDeals))
     print("Input format: col row, or 0 for helpdeck, 1 to give up")
 
 # check if coordinates are within bounds
 def invalidCoord(coord, nMaxCards):
     return len(coord) != 2 or coord[0] >= nTotalColumns or coord[0] < 0 or coord[1] >= len(t_columns[coord[0]]) or coord[1] < 0
 
-# trim the whitespace at the edges, split by space to get each separate coordinate, subtract one because list indexes begin at 0, input begins at 1
+# trim the whitespace at the edges, split by space to get each separate coordinate
+# subtract one because list indexes begin at 0, input begins at 1
 def getInput(inputText):
     return [int(c) - 1 for c in input(inputText).strip().split(" ")]
 
@@ -67,11 +71,12 @@ def gameOver():
         return True
     return False
 
-# this is where the game itself begins
+# this is where the game itself begins -------------------------------------------------------------------------------------------
 
 nCompletedUnits = 0
-nHelpDecks = nTotalHelpDecks
+nDeals = nTotalDeals
 
+clearScreen()
 print("How many suits do you want to play with? \nOptions are: one (1), two (2), or four (4)")
 nNumberOfSuits = int(input())
 
@@ -81,8 +86,7 @@ while nNumberOfSuits not in [1, 2, 4]:
     nNumberOfSuits = int(input())
 
 # based on the number of suits chosen we run through them enough times
-# to get equal amounts of units of each available suit 
-# then generate cards
+# to get an equal amount of units of each available suit then generate cards
 # we take the indexes of the suits and ranks for easier comparison between cards
 for i in range(nNumberOfSuits):                     
     for j in range(nTotalUnits // nNumberOfSuits):
@@ -91,12 +95,12 @@ for i in range(nNumberOfSuits):
 
 random.shuffle(t_allCards)
 
-# adding cards to the help decks
-for i in range(nTotalHelpDecks):
-    t_helpDecks.append([])
+# adding cards to the deals
+for i in range(nTotalDeals):
+    t_deals.append([])
     for j in range(10):
-        # to increase randomness, we take 10 random cards for the helper decks
-        t_helpDecks[i].append(t_allCards.pop(random.randint(0, len(t_allCards) - 1)))
+        # to increase randomness, we take 10 random cards for the deal decks
+        t_deals[i].append(t_allCards.pop(random.randint(0, len(t_allCards) - 1)))
 
 # adding cards to the playing columns
 # first we create a two-dimensional array by appending arrays to t_columns
@@ -115,23 +119,23 @@ for col in t_columns:
 
 # gameloop
 display()
-usedHelpdeck = False
+bUsedDeal = False
 
 while not gameOver():
-    if usedHelpdeck:
-        if nHelpDecks > 0:
-            print("Used a help deck")
+    if bUsedDeal:
+        if nDeals > 0:
+            print("Used a deal")
         else:
-            print("No more help decks to use")
-        usedHelpdeck = False
+            print("No more deals")
+        bUsedDeal = False
     
     # first col and then row, more intuitive imo
     coord = getInput("Input: ")
 
     if coord == [-1]:
-        usedHelpdeck = True
-        if nHelpDecks > 0:
-            nHelpDecks -= 1
+        bUsedDeal = True
+        if nDeals > 0:
+            nDeals -= 1
             # use helpdeck here
     else:
         nMaxCards = max([len(col) for col in t_columns])
